b326875ec0a5d0ea8277809f0315ccca36fa356f
Elian Borda <eliancamiloalejandro@gmail.com>
2022-09-02 18:15:59 -0300

    Se agrego el punto 9 al 14

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -63,3 +63,66 @@ apariciones _ []       = 0
 apariciones x (y:ys)   =  if x==y 
                             then 1+(apariciones x ys)
                             else apariciones x ys
+------------------------------------------
+
+-- 9. Dados un número n y una lista xs, devuelve todos los elementos de xs que son menores a n.
+losMenoresA :: Int -> [Int] -> [Int]
+losMenoresA _ []     = []
+losMenoresA n (m:ms) =  if m<n 
+                            then m : losMenoresA n ms  
+                            else losMenoresA n ms
+
+------------------------------------------
+
+-- 10. Dados un número n y una lista de listas, devuelve la lista de aquellas listas que tienen más
+-- de n elementos.
+lasDeLongitudMayorA :: Int -> [[a]] -> [[a]]
+lasDeLongitudMayorA _ []     = []
+lasDeLongitudMayorA n (x:xs) = if (longitud x)>n 
+                                    then  x : lasDeLongitudMayorA n xs
+                                    else lasDeLongitudMayorA n xs
+
+------------------------------------------
+
+-- 11. Dados una lista y un elemento, devuelve una lista con ese elemento agregado al final de la
+-- lista.
+agregarAlFinal :: [a] -> a -> [a]
+agregarAlFinal [] y = y:[]
+agregarAlFinal (x:xs) y =  x : agregarAlFinal xs y 
+
+------------------------------------------
+
+-- 12. Dadas dos listas devuelve la lista con todos los elementos de la primera lista y todos los
+-- elementos de la segunda a continuación. Definida en Haskell como (++).
+agregar :: [a] -> [a] -> [a]
+agregar [] ys = ys
+agregar (x:xs) ys = x : agregar xs ys
+
+------------------------------------------
+
+-- 13. Dada una lista devuelve la lista con los mismos elementos de atrás para adelante. Definida
+-- en Haskell como reverse.
+reversa :: [a] -> [a]
+reversa []     = []
+reversa (x:xs) =  agregarAlFinal (reversa xs) x 
+
+------------------------------------------
+
+-- 14. Dadas dos listas de enteros, devuelve una lista donde el elemento en la posición n es el
+-- máximo entre el elemento n de la primera lista y de la segunda lista, teniendo en cuenta que
+-- las listas no necesariamente tienen la misma longitud.
+zipMaximos :: [Int] -> [Int] -> [Int]
+zipMaximos [] _          = []
+zipMaximos _ []          = []
+zipMaximos (x:xs) (y:ys) = maxDelPar (x,y) : zipMaximos xs ys
+
+maxDelPar :: (Int, Int) -> Int
+maxDelPar (n, m) =  if n > m
+                        then n
+                        else m
+
+------------------------------------------
+
+-- 15. Dada una lista devuelve el mínimo
+elMinimo :: Ord a => [a] -> a
+elMinimo [a] 
