442b9a02f8fdbad3030dbd6f09214cf90d25b782
Elian Borda <eliancamiloalejandro@gmail.com>
2022-09-02 19:44:54 -0300

    2. Recursion sobre numeros completa

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -125,3 +125,56 @@ elMinimo (x:[]) = x
 elMinimo (x:x2:xs) = if x<x2
                         then elMinimo (x:xs)
                         else elMinimo (x2:xs)
+
+------------------------------------------
+
+-- #####  2. Recursión sobre números #####
+
+------------------------------------------
+
+-- 1. Dado un número n se devuelve la multiplicación de este número y todos sus anteriores hasta
+-- llegar a 0. Si n es 0 devuelve 1. La función es parcial si n es negativo.
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = if n>=0
+                then n * factorial (n-1)
+                else error "El valor es negativo"
+
+------------------------------------------
+
+-- 2. Dado un número n devuelve una lista cuyos elementos sean los números comprendidos entre
+-- n y 1 (incluidos). Si el número es inferior a 1, devuelve la lista vacía.
+cuentaRegresiva :: Int -> [Int]
+cuentaRegresiva 0 = []
+cuentaRegresiva n = if n>=1
+                        then n : cuentaRegresiva (n-1)
+                        else []
+
+------------------------------------------
+
+-- 3. Dado un número n y un elemento e devuelve una lista en la que el elemento e repite n veces.
+repetir ::  Int -> a -> [a]
+repetir 0 _ = []
+repetir n x = x : repetir (n-1) x
+
+------------------------------------------
+
+-- 4. Dados un número n y una lista xs, devuelve una lista con los n primeros elementos de xs.
+-- Si la lista es vacía, devuelve una lista vacía.
+losPrimeros :: Int -> [a] -> [a]
+losPrimeros 0 _      = []
+losPrimeros _ []     = []
+losPrimeros n (x:xs) = x : losPrimeros (n-1) xs
+
+------------------------------------------
+
+-- 5. Dados un número n y una lista xs, devuelve una lista sin los primeros n elementos de lista
+-- recibida. Si n es cero, devuelve la lista completa.
+sinLosPrimeros :: Int -> [a] -> [a]
+sinLosPrimeros 0 xs = xs
+sinLosPrimeros _ [] = []
+sinLosPrimeros n (x:xs) = sinLosPrimeros (n-1) xs
+
+-------------------------------------------
+
+
