f8e8d1dfa419aacdc9b9e4589247c5de8c0b53a6
Elian Borda <eliancamiloalejandro@gmail.com>
2022-09-10 21:11:45 -0300

    v3.1.2

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -3,7 +3,9 @@
 -- 1.1. Celdas con bolitas
 
 data Color = Azul | Rojo
+    deriving Show
 data Celda = Bolita Color Celda | CeldaVacia
+    deriving Show
 
 celda1 = Bolita Rojo (Bolita Azul (Bolita Rojo (Bolita Azul CeldaVacia)))
 
@@ -21,6 +23,22 @@ esColor :: Color -> Color -> Bool
 esColor Azul Azul = True
 esColor Rojo Rojo = True
 esColor _ _       = False
+
+-- Dado un color y una celda, agrega una bolita de dicho color a la celda.
+poner :: Color -> Celda -> Celda
+poner c cel = Bolita c cel
+
+-- Dado un color y una celda, quita una bolita de dicho color de la celda. Nota: a diferencia de
+-- Gobstones, esta función es total.
+sacar :: Color -> Celda -> Celda
+sacar _ CeldaVacia       = CeldaVacia
+sacar c (Bolita col cel) = if esColor col c
+                              then cel
+                              else Bolita col (sacar c cel)
+
+
+
+
 
 
 
