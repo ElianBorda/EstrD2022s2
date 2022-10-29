18f470ad0abc7022a8b081a48e9d9cf20512c313
Elian Borda <eliancamiloalejandro@gmail.com>
2022-10-29 00:19:23 -0300

    Cambios

--- tp-9/tp-9-ej3/tp-9-ej3.cpp
+++ tp-9/tp-9-ej3/tp-9-ej3.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main(){
     Par p;
     p = consPar(10, 3);
-    cout << "Primer componente: " << p.x << endl; 
-    // cout << "Primer componente: " << fst(p) << endl; 
-    // cout << "Segundo componente: " << snd(p) << endl; 
-    // cout << "El maximo componente: " << maxDelPar(p) << endl; 
-    // cout << "Antes de swap: " << "(" << fst(p) << ", " << snd(p) << ")" << endl;
-    // cout << "despues de swap: " << "(" << fst(swap(p)) << ", " << snd(swap(p)) << ")" << endl; 
-    // cout << "Division de 10/3: " << "(" << fst(divisionYResto(10, 3)) << ", " << snd(divisionYResto(10, 3)) << ")" << endl; 
+    cout << "Primer componente: " << fst(p) << endl; 
+    cout << "Segundo componente: " << snd(p) << endl; 
+    cout << "El maximo componente: " << maxDelPar(p) << endl; 
+    cout << "Antes de swap: " << "(" << fst(p) << ", " << snd(p) << ")" << endl;
+    cout << "despues de swap: " << "(" << fst(swap(p)) << ", " << snd(swap(p)) << ")" << endl; 
+    cout << "Division de 10/3: " << "(" << fst(divisionYResto(10, 3)) << ", " << snd(divisionYResto(10, 3)) << ")" << endl; 
 }
