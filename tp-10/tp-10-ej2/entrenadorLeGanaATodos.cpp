90f24f52ff0222b212306bcd947a3d638bc43577
Elian Borda <eliancamiloalejandro@gmail.com>
2022-11-09 23:05:26 -0300

    cambios

--- tp-10/tp-10-ej2/entrenadorLeGanaATodos.cpp
+++ tp-10/tp-10-ej2/entrenadorLeGanaATodos.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int main(){
-    Pokemon* pokemones = new Pokemon[2];
+    Pokemon* pokemones = new Pokemon[3];
     pokemones[0] = consPokemon("Agua");
     pokemones[1] = consPokemon("Fuego");
-    pokemones[2] = consPokemon("Fuego");
 
     //Aun no esta terminado
+
+    cout << "poke: " << pokemones[0] << endl;
+    cout << "poke: " << pokemones[1] << endl;
+    cout << "poke: " << pokemones[2] << endl;
+    
 }
--- tp-10/tp-10-ej3/ArrayList.cpp
+++ tp-10/tp-10-ej3/ArrayList.cpp
@@ -8,3 +8,35 @@ struct ArrayListSt {
     int capacidad; // tamaño del array
 };
 
+ArrayList newArrayList(){
+    ArrayList xs = new ArrayListSt;
+    xs->cantidad = 0;
+    xs->elementos = new int[15];
+    xs->capacidad = 16;
+
+    return xs;
+}
+
+ArrayList newArrayListWith(int capacidad){
+    ArrayList xs = new ArrayListSt;
+    xs->cantidad = 0;
+    xs->elementos = new int[capacidad];
+    xs->capacidad = capacidad;
+
+    return xs;
+}
+
+int lengthAL(ArrayList xs){
+    return xs->cantidad;
+}
+
+int get(int i, ArrayList xs){
+    return xs->elementos[i];
+}
+
+void set(int i, int x, ArrayList xs){
+     xs->elementos[i] = x;
+}
+
+void resize(int capacidad, ArrayList xs){
+}
